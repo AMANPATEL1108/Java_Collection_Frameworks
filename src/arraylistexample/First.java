0e693046ba854437d346a89f033c93a35036c8b9
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-01-11 18:26:53 +0530

    1 Arraylist Completed

---
+++ src/arraylistexample/First.java
@@ -0,0 +1,36 @@
+package arraylistexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class First {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+//        System.out.println(list.get(1));
+//        System.out.println(list.size());
+//        for(int i=0;i< list.size();i++){
+//            System.out.println(list.get(i));
+//        }
+//
+//        for(int a:list){
+//            System.out.println((a));
+//        }
+//        System.out.println(list.contains(5));
+//    list.remove(2);
+//    list.add(2,5);
+
+//        for(int a:list){
+//            System.out.println((a));
+//        }
+
+        System.out.println(list);
+
+//
+
+
+    }
+}
