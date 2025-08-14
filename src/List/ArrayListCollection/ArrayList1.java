4fc46f463678920a01e07f991215bcd48dc8d452
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-14 09:02:24 +0530

    Done

--- src/List/ArrayListCollection/ArrayList1.java
+++ src/List/ArrayListCollection/ArrayList1.java
@@ -8,27 +8,27 @@ public class ArrayList1 {
         list.add(1);
         list.add(2);
         list.add(3);
-//        System.out.println(list.get(2));
-//        System.out.println(list.size());
+        System.out.println(list.get(2));
+        System.out.println(list.size());
 
-//        for (int i=0;i<list.size();i++){
-//            System.out.println(list.get(i));
-//        }
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
 
-//        for(int x:list){
-//            System.out.println(x);
-//        }
+        for(int x:list){
+            System.out.println(x);
+        }
 
-//        System.out.println( list.contains(5));
-//        System.out.println( list.contains(1));
-//        list.remove(2);
-//        for(int y:list){
-//            System.out.println(y);
-//        }
+        System.out.println( list.contains(5));
+        System.out.println( list.contains(1));
+        list.remove(2);
+        for(int y:list){
+            System.out.println(y);
+        }
 
 
 
-//        list.add(2,50);
+        list.add(2,50);
 
         list.set(2,150);
 
